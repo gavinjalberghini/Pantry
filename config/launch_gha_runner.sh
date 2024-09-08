2bfc8ea235c74a196171d66a16d87141599e1ecc
Gavin Alberghini <gavinjalberghini@gmail.com>
2024-09-08 04:50:37 -0400

    feat(gha launch script): Added an additional script to launch GitHub Actions runner. Also renamed ubuntu install script.

---
+++ config/launch_gha_runner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Navigate to the actions-runner directory
+cd ~/actions-runner || { echo "Directory ~/actions-runner not found."; exit 1; }
+
+# Execute the run.sh script
+./run.sh
+
