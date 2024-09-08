03856b5a51b6f278180f62897bbb21c7a8c550ee
Gavin Alberghini <gavinjalberghini@gmail.com>
2024-09-08 05:33:40 -0400

    feat(gha launcher): Updated GHA launcher to be silent and to log to a file.

--- config/launch_gha_runner.sh
+++ config/launch_gha_runner.sh
@@ -3,8 +3,18 @@
 # Navigate to the actions-runner directory
 cd ~/actions-runner || { echo "Directory ~/actions-runner not found."; exit 1; }
 
-# Execute the run.sh script in the background
-./run.sh &
+# Check if the script is already running
+if pgrep -f 'run.sh' > /dev/null; then
+	  echo "run.sh is already running." 
+	    exit 1
+fi
 
-# Optionally, you can capture the process ID (PID) and log it
+# Define log file name with a timestamp
+log_file="/home/galberghini/actions-runner/runner.log"
+date_time="$(date +'%Y-%m-%d_%H-%M-%S')"
+echo "$date_time : Launched runner service.." >> "$log_file"
+
+# Execute the run.sh script in the background
+./run.sh >> "$log_file" 2>&1 &
 echo "Started ./run.sh with PID $!"
+
