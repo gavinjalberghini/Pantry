dd25c5c31edf232628a0b2fb49c44b213acee095
Gavin Alberghini <gavinjalberghini@gmail.com>
2024-09-18 22:02:17 -0400

    refactor(deps script): Update deps script to be more robust

--- config/ubuntu_install_deps.sh
+++ config/ubuntu_install_deps.sh
@@ -25,9 +25,9 @@ function error() {
 }
 
 notify "Installing latest git..."
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt update
-sudo apt install git
+sudo add-apt-repository ppa:git-core/ppa -y > /dev/null 2>&1
+sudo apt -qq update
+sudo apt -qq install git -y
 
 if ! command -v docker &> /dev/null; then 
   notify "Installing docker..."
@@ -38,65 +38,55 @@ if ! command -v docker &> /dev/null; then
   sudo apt install --yes docker-ce
   sudo systemctl status docker --no-page
   notify "You will need to restart your instance for docker to work for you..."
+  sudo usermod -aG docker $USER
+  sudo newgrp docker
 else
   notify "Docker already installed."
 fi
 
-sudo usermod -aG docker $USER
-newgrp docker
-
 notify "Installing kubectl..."
-curl -SsL https://dl.k8s.io/release/v1.21.0/bin/$(uname |  tr '[:upper:]' '[:lower:]')/$(dpkg --print-architecture)/kubectl -o kubectl
-sudo mv kubectl /usr/local/bin
+curl -SsL https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/$(uname |  tr '[:upper:]' '[:lower:]')/$(dpkg --print-architecture)/kubectl -o kubectl
+sudo mv -f kubectl /usr/local/bin
 sudo chmod +x /usr/local/bin/kubectl
 
 notify "Installing helm..."
 curl -SsLO https://get.helm.sh/helm-v3.8.2-$(uname |  tr '[:upper:]' '[:lower:]')-$(dpkg --print-architecture).tar.gz
-tar -zxvf helm-v3.8.2-$(uname |  tr '[:upper:]' '[:lower:]')-$(dpkg --print-architecture).tar.gz
-sudo mv linux-$(dpkg --print-architecture)/helm /usr/local/bin
+tar -zxf helm-v3.8.2-$(uname |  tr '[:upper:]' '[:lower:]')-$(dpkg --print-architecture).tar.gz
+sudo mv -f linux-$(dpkg --print-architecture)/helm /usr/local/bin
 sudo chmod +x /usr/local/bin/helm
 rm -Rf linux-$(dpkg --print-architecture)
 rm helm-v3.8.2-$(uname |  tr '[:upper:]' '[:lower:]')-$(dpkg --print-architecture).tar.gz
 
 notify "Installing kustomize..."
 curl -SsL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.5.7/kustomize_v4.5.7_linux_$(dpkg --print-architecture).tar.gz | tar xz -C /tmp
-sudo mv /tmp/kustomize /usr/local/bin
+sudo mv -f /tmp/kustomize /usr/local/bin
 
 notify "Installing chef-inspec..."
-sudo curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
+sudo curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec > /dev/null 2>&1
 
 notify "Installing terraform..."
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt install -y unzip
+sudo apt-get -qq update && sudo apt-get -qq install -y gnupg software-properties-common
+sudo apt -qq install -y unzip
 curl -SsLO https://releases.hashicorp.com/terraform/1.3.6/terraform_1.3.6_$(uname | tr '[:upper:]' '[:lower:]')_$(dpkg --print-architecture).zip
-unzip terraform_1.3.6_$(uname | tr '[:upper:]' '[:lower:]')_$(dpkg --print-architecture).zip
-sudo mv terraform /usr/local/bin/terraform
+unzip -qq terraform_1.3.6_$(uname | tr '[:upper:]' '[:lower:]')_$(dpkg --print-architecture).zip
+sudo mv -f terraform /usr/local/bin/terraform
 sudo chmod +x /usr/local/bin/terraform
 rm terraform_1.3.6_$(uname | tr '[:upper:]' '[:lower:]')_$(dpkg --print-architecture).zip
 
 notify "Installing packer..."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository -y "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update
-sudo apt-get install packer
+sudo apt-add-repository -y "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /dev/null 2>&1
+sudo apt-get -qq update
+sudo apt-get -qq install packer
 
 notify "Installing terraform docs..."
 curl -sSLo ./terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v0.17.0/terraform-docs-v0.17.0-$(uname)-$(dpkg --print-architecture).tar.gz
 tar -xzf terraform-docs.tar.gz
 chmod +x terraform-docs
-sudo mv terraform-docs /usr/local/bin/terraform-docs
+sudo mv -f terraform-docs /usr/local/bin/terraform-docs
 rm terraform-docs.tar.gz
 
 notify "Installing sops..."
 curl -SsLO https://github.com/mozilla/sops/releases/download/v3.7.3/sops-v3.7.3.$(uname | tr '[:upper:]' '[:lower:]').$(dpkg --print-architecture)
-sudo mv sops-v3.7.3.$(uname |  tr '[:upper:]' '[:lower:]').$(dpkg --print-architecture) /usr/local/bin/sops
+sudo mv -f sops-v3.7.3.$(uname |  tr '[:upper:]' '[:lower:]').$(dpkg --print-architecture) /usr/local/bin/sops
 sudo chmod +x /usr/local/bin/sops
